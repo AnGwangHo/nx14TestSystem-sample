7d91266a49272428335da224cff7de47b4f9694f
AnGwangHo <akh4444@nate.com>
2020-02-24 15:36:11 +0900

    auto push(file upload)

---
+++ 86363/17/RP_86363_TEST0731_1.xfdl.js
@@ -0,0 +1,139 @@
+﻿(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        this.on_create = function()
+        {
+            // Declare Reference
+            var obj = null;
+            
+            if (Form == this.constructor) {
+                this.set_name("TEST0731_1");
+                this.set_titletext("New Form");
+                this._setFormPosition(0,0,1024,768);
+            }
+
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+
+            
+            // UI Components Initialize
+            obj = new Div("Div00", "absolute", "11", "157", "570", "342", null, null, this);
+            obj.set_taborder("0");
+            obj.set_text("Div00");
+            obj.set_url("Base::RP_86363_TEST0731_2.xfdl");
+            this.addChild(obj.name, obj);
+
+            obj = new Static("Static02", "absolute", "11", "56", "331", "82", null, null, this);
+            obj.set_taborder("2");
+            obj.set_text("테스트 방법\r\n\r\n 1. 화면 로드 후 Div내 Cell 클릭\r\n 2. 콘솔창에서 trace 확인\r\n 3. '222'가 콘솔창에 출력되는지 확인");
+            obj.style.set_align("left top");
+            obj.style.set_font("10 Dotum");
+            this.addChild(obj.name, obj);
+
+            obj = new Static("Static01", "absolute", "11", "7", "413", "37", null, null, this);
+            obj.set_taborder("3");
+            obj.set_text("RP 86363 transaction 후 cellclick 발생 안하는 현상");
+            obj.style.set_color("blueviolet");
+            obj.style.set_font("verdana,10,bold");
+            this.addChild(obj.name, obj);
+
+
+            
+            // Layout Functions
+            //-- Default Layout
+            obj = new Layout("default", "", 1024, 768, this,
+            	//-- Layout function
+            	function(p) {
+            		p.set_titletext("New Form");
+
+            	}
+            );
+            this.addLayout(obj.name, obj);
+
+
+            
+            // BindItem Information
+
+            
+            // Remove Reference
+            obj = null;
+        };
+        
+        this.loadPreloadList = function()
+        {
+            this._addPreloadList("fdl", "Base::RP_86363_TEST0731_2.xfdl");
+        };
+        
+        // User Script
+        this.registerScript("RP_86363_TEST0731_1.xfdl", function() {
+
+        this.TEST0731_1_onlbuttondown = function(obj,e)
+        {
+        	trace('11');
+        	this.fn_chkWrisStop();
+        }
+
+        
+
+        
+        /*******************************************************************************
+         * fn_chkWrisStop : 통합정보시스템 이용 제한 여부 확인
+         ********************************************************************************/
+        this.fn_chkWrisStop = function()
+        {
+        	var url = "jsp::RP_86363_NC_NoSQL2.jsp";
+        	this.transaction( "fn_chkWrisStop"
+        					, url
+        					, ""
+        					, ""
+        					, "a=b"
+        					, "fn_trans_callback"
+        					, false
+        					, 0
+        					, false);
+        }
+
+        /***************************************************************************************
+         * fn_trans_callback
+         ***************************************************************************************/
+        this.fn_trans_callback = function (sSvcId,nErrorCode,sReturn)
+        {
+        	switch (sSvcId)
+        	{
+        		case "fn_chkWrisStop":
+        			if (nErrorCode == 1)
+        			{
+        				trace('4444444444');
+        			}
+        			else
+        			{
+        				trace('333333333');
+        			}
+        			break;
+
+        		default:
+        			break;
+        	}
+        }
+        });
+
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+            this.addEventHandler("onlbuttondown", this.TEST0731_1_onlbuttondown, this);
+            this.Static02.addEventHandler("onclick", this.Static02_onclick, this);
+
+        };
+
+        this.loadIncludeScript("RP_86363_TEST0731_1.xfdl");
+        this.loadPreloadList();
+       
+    };
+}
+)();
---
+++ 86363/17/RP_86363_TEST0731_2.xfdl.js
@@ -0,0 +1,91 @@
+﻿(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        this.on_create = function()
+        {
+            // Declare Reference
+            var obj = null;
+            
+            if (Form == this.constructor) {
+                this.set_name("TEST0731");
+                this.set_titletext("New Form");
+                this._setFormPosition(0,0,600,393);
+            }
+
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+            obj = new Dataset("Dataset00", this);
+            obj._setContents("<ColumnInfo><Column id=\"Column0\" type=\"STRING\" size=\"256\"/><Column id=\"Column1\" type=\"STRING\" size=\"256\"/><Column id=\"Column2\" type=\"STRING\" size=\"256\"/><Column id=\"Column3\" type=\"STRING\" size=\"256\"/></ColumnInfo><Rows><Row><Col id=\"Column0\">1</Col></Row><Row><Col id=\"Column0\">2</Col></Row><Row><Col id=\"Column0\">3</Col></Row><Row><Col id=\"Column0\">4</Col></Row></Rows>");
+            this.addChild(obj.name, obj);
+
+
+            
+            // UI Components Initialize
+            obj = new Grid("Grid00", "absolute", "6.83%", "63", null, "279", "18.5%", null, this);
+            obj.set_taborder("0");
+            obj.set_binddataset("Dataset00");
+            obj._setContents("<Formats><Format id=\"default\"><Columns><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/></Columns><Rows><Row band=\"head\" size=\"24\"/><Row band=\"body\" size=\"24\"/></Rows><Band id=\"head\"><Cell col=\"0\" disptype=\"normal\" text=\"Column0\"/><Cell col=\"1\" disptype=\"normal\" text=\"Column1\"/><Cell col=\"2\" disptype=\"normal\" text=\"Column2\"/><Cell col=\"3\" disptype=\"normal\" text=\"Column3\"/></Band><Band id=\"body\"><Cell col=\"0\" disptype=\"normal\" text=\"bind:Column0\"/><Cell col=\"1\" disptype=\"normal\" text=\"bind:Column1\"/><Cell col=\"2\" disptype=\"normal\" text=\"bind:Column2\"/><Cell col=\"3\" disptype=\"normal\" text=\"bind:Column3\"/></Band></Format></Formats>");
+            this.addChild(obj.name, obj);
+
+            obj = new Static("Static01", "absolute", "11", "7", "413", "37", null, null, this);
+            obj.set_taborder("1");
+            obj.set_text("RP 86363 transaction 후 cellclick 발생 안하는 현상");
+            obj.style.set_color("blueviolet");
+            obj.style.set_font("verdana,10,bold");
+            this.addChild(obj.name, obj);
+
+
+            
+            // Layout Functions
+            //-- Default Layout
+            obj = new Layout("default", "", 600, 393, this,
+            	//-- Layout function
+            	function(p) {
+            		p.set_titletext("New Form");
+
+            	}
+            );
+            this.addLayout(obj.name, obj);
+
+
+            
+            // BindItem Information
+
+            
+            // Remove Reference
+            obj = null;
+        };
+        
+
+        
+        // User Script
+        this.registerScript("RP_86363_TEST0731_2.xfdl", function() {
+
+        this.Grid00_oncellclick = function(obj,e)
+        {
+        	trace('222');
+        }
+
+        
+        
+        });
+
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+            this.Grid00.addEventHandler("oncellclick", this.Grid00_oncellclick, this);
+
+        };
+
+        this.loadIncludeScript("RP_86363_TEST0731_2.xfdl");
+
+       
+    };
+}
+)();
