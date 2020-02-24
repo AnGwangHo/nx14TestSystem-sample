f5beb377b90a82bbb2e06e689ab5269d7a329cd9
AnGwangHo <akh4444@nate.com>
2020-02-24 16:18:37 +0900

    auto push(file upload)

---
+++ 84261/34/RP_84261_tab.xfdl.js
@@ -0,0 +1,87 @@
+﻿(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        this.on_create = function()
+        {
+            // Declare Reference
+            var obj = null;
+            
+            if (Form == this.constructor) {
+                this.set_name("tab");
+                this.set_titletext("New Form");
+                this._setFormPosition(0,0,1024,768);
+            }
+
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+
+            
+            // UI Components Initialize
+            obj = new Tab("Tab00", "absolute", "4.39%", "151", null, "265", "56.93%", null, this);
+            obj.set_taborder("0");
+            obj.set_tabindex("0");
+            obj.set_scrollbars("autoboth");
+            this.addChild(obj.name, obj);
+            obj = new Tabpage("tabpage1", this.Tab00);
+            obj.set_text("tabpage1");
+            this.Tab00.addChild(obj.name, obj);
+            obj = new Tabpage("tabpage2", this.Tab00);
+            obj.set_text("tabpage2");
+            this.Tab00.addChild(obj.name, obj);
+
+            obj = new Static("Static01", "absolute", "3", "7", "547", "37", null, null, this);
+            obj.set_taborder("1");
+            obj.set_text("RP 84261 Tabpage의 border가 두꺼워지는 현상");
+            obj.style.set_color("blueviolet");
+            obj.style.set_font("verdana,10,bold");
+            this.addChild(obj.name, obj);
+
+            obj = new Static("Static02", "absolute", "0.2%", "52", null, "92", "0.88%", null, this);
+            obj.set_taborder("2");
+            obj.set_text("테스트 방법\r\n\r\n 1. TabButton쪽 Tabpage의 border가 다른 쪽 border에 비해 두꺼운 것을 확인");
+            obj.style.set_font("10 Dotum");
+            this.addChild(obj.name, obj);
+
+
+            
+            // Layout Functions
+            //-- Default Layout
+            obj = new Layout("default", "", 1024, 768, this,
+            	//-- Layout function
+            	function(p) {
+            		p.set_titletext("New Form");
+
+            	}
+            );
+            this.addLayout(obj.name, obj);
+
+
+            
+            // BindItem Information
+
+            
+            // Remove Reference
+            obj = null;
+        };
+        
+
+        
+        // User Script
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+
+        };
+
+        this.loadIncludeScript("RP_84261_tab.xfdl");
+
+       
+    };
+}
+)();
